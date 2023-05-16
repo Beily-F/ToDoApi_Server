2c4940556ccb6fe05846c0c5c0f2ed51407e29b9
משתמש זה < @DESKTOP-SN2JHFI>
2023-05-16 11:37:16 +0300

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoApi;
+
+public partial class Item
+{
+    public int Id { get; set; }
+
+    public string? Namw { get; set; }
+
+    public bool? IsComplete { get; set; }
+}
